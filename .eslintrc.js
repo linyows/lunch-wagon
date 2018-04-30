ecbc2d9df16cbb16c9e5006bfb7505f134463f1a
linyows <linyows@gmail.com>
2018-04-30 22:38:40 +0900

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "extends": "google",
+  "parserOptions": {
+    "ecmaVersion": 3,
+  },
+  "env": {
+    "node": true
+  },
+  "rules": {
+    "max-len": ["error", {"code": 100}],
+    "camelcase": "off",
+    "no-var": "off",
+    "no-unused-vars": "off",
+    "indent": [ "error", 2 ],
+    "linebreak-style": [ "error", "unix" ],
+    "quotes": [ "error", "single" ],
+    "semi": [ "error", "never" ]
+  }
+};
